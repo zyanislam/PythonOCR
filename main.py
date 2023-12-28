7bfb28c770bdbe32a1f11fd1575527e57f5fb28f
Zyan <sheikh.zyanislam@gmail.com>
2023-12-28 17:44:50 +0600

    Able to read all pdfs in the directory

--- main.py
+++ main.py
@@ -3,95 +3,73 @@ import os
 import cv2
 import pytesseract
 import numpy as np
-
 from pdf2image import convert_from_path
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 pytesseract.pytesseract.tesseract_cmd = text = r'/usr/bin/tesseract'
 
-pdf_path = "/home/quantum/Downloads/Projects/PythonOCR/pdfs/Resume - Rezab Ud Dawla.pdf"
-images = convert_from_path(pdf_path)
-
+pdfs_directory = "/home/quantum/Downloads/Projects/PythonOCR/pdfs"
 output_folder = "images"
 
-result_dict = {}
-for i, img in enumerate(images):
-    img_rgb = cv2.cvtColor(cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR), cv2.COLOR_BGR2RGB)
-    text = pytesseract.image_to_string(img_rgb)
-    result_dict[f"page_{i+1}"] = text
+# Create the "images" folder if it doesn't exist
+os.makedirs(output_folder, exist_ok=True)
 
-    # Save the image to the "images" folder
-    img.save(os.path.join(output_folder, f"page_{i+1}.png"))
+# List all PDF files in the "pdfs" directory
+pdf_files = [f for f in os.listdir(pdfs_directory) if f.endswith('.pdf')]
 
-image1 = cv2.imread("images/image_1.png")
-image2 = cv2.imread("images/image_2.png")
-image3 = cv2.imread("images/page_1.png")
-image4 = cv2.imread("images/page_2.png")
+# Create a list to store the OCR results and images for all PDFs
+all_ocr_results = []
 
-img_1_rgb = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
-img_2_rgb = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
-img_3_rgb = cv2.cvtColor(image3, cv2.COLOR_BGR2RGB)
-img_4_rgb = cv2.cvtColor(image4, cv2.COLOR_BGR2RGB)
+# Loop through each PDF file
+for pdf_file in pdf_files:
+    pdf_path = os.path.join(pdfs_directory, pdf_file)
 
-print(pytesseract.image_to_string(img_1_rgb))
-print(pytesseract.image_to_string(img_2_rgb))
-print(pytesseract.image_to_string(img_3_rgb))
-print(pytesseract.image_to_string(img_4_rgb))
+    # Convert PDF to images
+    images = convert_from_path(pdf_path)
 
-text_1 = pytesseract.image_to_string(img_1_rgb)
-text_2 = pytesseract.image_to_string(img_2_rgb)
-text_3 = pytesseract.image_to_string(img_3_rgb)
-text_4 = pytesseract.image_to_string(img_4_rgb)
+    result_dict = {}
+    for i, img in enumerate(images):
+        img_rgb = cv2.cvtColor(cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR), cv2.COLOR_BGR2RGB)
+        text = pytesseract.image_to_string(img_rgb)
+        result_dict[f"page_{i+1}"] = text
 
-# Create a dictionary to store the OCR results
-result_dict = {
-    "image_1": text_1,
-    "image_2": text_2,
-    "image_3": text_3,
-    "image_4": text_4
-}
+        # Save the image to the "images" folder
+        img.save(os.path.join(output_folder, f"{pdf_file}_page_{i+1}.png"))
 
-# Convert the dictionary to JSON
-json_data = json.dumps(result_dict, indent=2)
+    # Create a list to store the OCR results and images for the current PDF
+    ocr_results = []
+    for key, value in result_dict.items():
+        img_path = os.path.join(output_folder, f"{pdf_file}_page_{key}.png")
+        ocr_results.append({"page": key, "text": value, "img_path": img_path, "pdf_file": pdf_file})
 
-# Print or save the JSON data
-print("JSON Data:")
-print(json_data)
+    # Extend the list of OCR results for all PDFs
+    all_ocr_results.extend(ocr_results)
+
+# Convert the list to JSON for all PDFs
+json_data_all_pdfs = json.dumps(all_ocr_results, indent=2)
+
+# Print or save the JSON data for all PDFs
+print("JSON Data for All PDFs:")
+print(json_data_all_pdfs)
 
 # Prompt the user for keywords
 user_keywords = input("Enter keywords (comma-separated): ")
 user_keywords_list = user_keywords.split(',')
 
-# Calculate cosine similarity and filter the JSON data
-filtered_data = {}
-vectorizer = CountVectorizer().fit_transform([text_1, text_2, text_3, text_4] + user_keywords_list)
-cosine_similarities = cosine_similarity(vectorizer)
+# Calculate cosine similarity and score the OCR results for all PDFs
+vectorizer_all_pdfs = CountVectorizer().fit_transform([result["text"] for result in all_ocr_results] + user_keywords_list)
+cosine_similarities_all_pdfs = cosine_similarity(vectorizer_all_pdfs)
 
-for i, key in enumerate(result_dict.keys()):
-    similarity_score = cosine_similarities[i][-len(user_keywords_list):]
-    if any(score > 0 for score in similarity_score):
-        filtered_data[key] = result_dict[key]
+# Score the OCR results based on cosine similarity for all PDFs
+for i, result in enumerate(all_ocr_results):
+    similarity_score_all_pdfs = cosine_similarities_all_pdfs[i][-len(user_keywords_list):]
+    result["score"] = sum(similarity_score_all_pdfs)
 
-filtered_json_data = json.dumps(filtered_data, indent=2)
+# Sort OCR results by the score in descending order for all PDFs
+sorted_ocr_results_all_pdfs = sorted(all_ocr_results, key=lambda x: x["score"], reverse=True)
 
-print("\nFiltered JSON Data:")
-print(filtered_json_data)
-
-# with open("output.json", "w") as json_file:
-#     json_file.write(json_data)
-
-# result = pytesseract.image_to_boxes(img_1_rgb)
-# ih, iw, ic = image1.shape
-# for box in result.splitlines():
-#     box = box.split(' ')
-#     print(box)
-#
-#     x, y, w, h = int(box[1]), int(box[2]), int(box[3]), int(box[4])
-#     cv2.rectangle(image1, (x, ih-y), (w, ih-h), (0, 255, 0), 2)
-
-# cv2.imshow("Input", image1)
-# cv2.waitKey(0)
-#
-# cv2.imshow("Input", image2)
-# cv2.waitKey(0)
+# Display only the names of the pages in the sorted JSON data for all PDFs
+print("\nSorted Pages for All PDFs:")
+for result in sorted_ocr_results_all_pdfs:
+    print(f"Page: {result['page']} | Score: {result['score']} | PDF: {result['pdf_file']}")
